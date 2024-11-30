30b770677d88f6c7d4b12eeef97ed059c93e0aa7
Annomy111 <Fedo.hagge-kubat@fu-berlin.de>
2024-11-30 14:31:17 +0100

    Fix middleware structure in task routes

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -4,60 +4,59 @@ const taskController = require('../controllers/taskController');
 const auth = require('../middleware/auth');
 const checkTaskAccess = require('../middleware/checkTaskAccess');
 const validate = require('../middleware/validate');
-const cache = require('../middleware/cache');
 const taskValidation = require('../validation/taskValidation');
 const errorHandler = require('../middleware/errorHandler');
 const config = require('../config/constants');
 
 // Base routes
-router.get('/', 
+router.get('/', [
   auth.protect,
   validate(taskValidation.getTasks),
   checkTaskAccess,
   errorHandler(taskController.getAllTasks)
-);
+]);
 
-router.post('/',
+router.post('/', [
   auth.protect,
   validate(taskValidation.createTask),
   checkTaskAccess,
   errorHandler(taskController.createTask)
-);
+]);
 
-router.get('/:id',
+router.get('/:id', [
   auth.protect,
   validate(taskValidation.getTask),
   checkTaskAccess,
   errorHandler(taskController.getTask)
-);
+]);
 
-router.patch('/:id',
+router.patch('/:id', [
   auth.protect,
   validate(taskValidation.updateTask),
   checkTaskAccess,
   errorHandler(taskController.updateTask)
-);
+]);
 
-router.delete('/:id',
+router.delete('/:id', [
   auth.protect,
   validate(taskValidation.deleteTask),
   checkTaskAccess,
   errorHandler(taskController.deleteTask)
-);
+]);
 
 // Special actions
-router.patch('/:id/progress',
+router.patch('/:id/progress', [
   auth.protect,
   validate(taskValidation.updateProgress),
   checkTaskAccess,
   errorHandler(taskController.updateTaskProgress)
-);
+]);
 
-router.patch('/:id/toggle-complete',
+router.patch('/:id/toggle-complete', [
   auth.protect,
   validate(taskValidation.updateTask),
   checkTaskAccess,
   errorHandler(taskController.toggleTaskComplete)
-);
+]);
 
 module.exports = router;

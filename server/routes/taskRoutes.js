6b0c3b57ee54b7d168675331ab957f187c67de54
Annomy111 <Fedo.hagge-kubat@fu-berlin.de>
2024-11-30 14:26:16 +0100

    Fix taskRoutes middleware composition

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -10,46 +10,55 @@ const errorHandler = require('../middleware/errorHandler');
 const config = require('../config/constants');
 
 // Middleware composition
-const taskMiddleware = (validationSchema, ttl) => [
-  auth.protect,
-  validate(validationSchema),
-  checkTaskAccess,
-  ttl ? cache.route({ ttl }) : (req, res, next) => next()
-];
+const taskMiddleware = (validationSchema, ttl) => {
+  const middlewares = [auth.protect];
+  
+  if (validationSchema) {
+    middlewares.push(validate(validationSchema));
+  }
+  
+  middlewares.push(checkTaskAccess);
+  
+  if (ttl) {
+    middlewares.push(cache.route({ ttl }));
+  }
+  
+  return middlewares;
+};
 
 // Base routes
 router.route('/')
   .get(
-    taskMiddleware(taskValidation.getTasks, config.cache.defaultTTL),
+    ...taskMiddleware(taskValidation.getTasks, config.cache.defaultTTL),
     errorHandler(taskController.getAllTasks)
   )
   .post(
-    taskMiddleware(taskValidation.createTask),
+    ...taskMiddleware(taskValidation.createTask),
     errorHandler(taskController.createTask)
   );
 
 router.route('/:id')
   .get(
-    taskMiddleware(taskValidation.getTask, config.cache.shortTTL),
+    ...taskMiddleware(taskValidation.getTask, config.cache.shortTTL),
     errorHandler(taskController.getTask)
   )
   .patch(
-    taskMiddleware(taskValidation.updateTask),
+    ...taskMiddleware(taskValidation.updateTask),
     errorHandler(taskController.updateTask)
   )
   .delete(
-    taskMiddleware(taskValidation.deleteTask),
+    ...taskMiddleware(taskValidation.deleteTask),
     errorHandler(taskController.deleteTask)
   );
 
 // Special actions
 router.patch('/:id/progress',
-  taskMiddleware(taskValidation.updateProgress),
+  ...taskMiddleware(taskValidation.updateProgress),
   errorHandler(taskController.updateTaskProgress)
 );
 
 router.patch('/:id/toggle-complete',
-  taskMiddleware(taskValidation.toggleComplete),
+  ...taskMiddleware(taskValidation.toggleComplete),
   errorHandler(taskController.toggleTaskComplete)
 );
 
